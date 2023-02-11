2610aa763c3f4612bf5e2a5ef556b66a1b308812
thomasfregene <fregzythomas@gmail.com>
2023-02-11 09:10:30 +0100

    completed CRUD Operations discount Grpc Service

--- src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -1,6 +1,14 @@
-﻿namespace Discount.Grpc.Mapper
+﻿using AutoMapper;
+using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Mapper
 {
-    public class DiscountProfile
+    public class DiscountProfile : Profile
     {
+        public DiscountProfile()
+        {
+            CreateMap<Coupon, CouponModel>().ReverseMap();
+        }
     }
 }
--- src/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Services/Discount/Discount.Grpc/Startup.cs
@@ -18,8 +18,10 @@ namespace Discount.Grpc
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
             services.AddScoped<IDiscountRepository, DiscountRepository>();
+            services.AddAutoMapper(typeof(Startup));
+            services.AddGrpc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
