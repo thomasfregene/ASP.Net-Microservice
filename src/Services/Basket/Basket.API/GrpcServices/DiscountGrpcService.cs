158842126e8632cdd62b9a4a6c530c895232d887
thomasfregene <fregzythomas@gmail.com>
2023-02-21 04:02:08 +0100

    consuming discount grpc service in basket api contd

--- src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -1,6 +1,22 @@
-﻿namespace Basket.API.GrpcServices
+﻿using Discount.Grpc.Protos;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
 {
     public class DiscountGrpcService
     {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProto;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProto)
+        {
+            _discountProto = discountProto;
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountProto.GetDiscountAsync(discountRequest);
+        }
     }
 }
