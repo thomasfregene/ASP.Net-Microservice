c951195448aec7e552a4c7edef31974a02537503
thomasfregene <fregzythomas@gmail.com>
2023-03-09 02:06:46 +0100

    added application behaviours- Application Layer

--- src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -8,7 +8,7 @@ namespace Ordering.Application.Exceptions
 {
     public class NotFoundException : ApplicationException
     {
-        public NotFoundException(string name, int key)
+        public NotFoundException(string name, object key)
             :base($"Entity \"{name}\" ({key}) was not found.") 
         { }
     }
