fc333e1606367091453a6eb4e939fa98417a4a30
thomasfregene <fregzythomas@gmail.com>
2023-03-10 00:30:33 +0100

    implementing persistence infrastructure -ordering.Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any()) 
+            {
+                orderContext.AddRange(GetPreConfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>()
+            {
+                new Order() {UserName="swn", FirstName="John", LastName="Doe", EmailAddress="fregzythomas@gmail.com",
+                    AddressLine="Oladele Olashore, V.I", Country="NIGERIA", TotalPrice=350}
+            };
+        }
+    }
+}
